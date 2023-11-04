6938ea945d052fd8a7695544d74133ea662ad282
epronk88 <erik.pronk88@gmail.com>
2023-11-04 16:19:27 +0100

    0411_17

--- test.py
+++ test.py
@@ -21,41 +21,39 @@ def parser():
     client = tweepy.Client(bearer_token,key,keySecret,accessToken,accessTokenSecret)
     
 
-    feed = feedparser.parse("https://alarmeringen.nl/feeds/region/rotterdam-rijnmond.rss").entries
+    feed = feedparser.parse("https://alarmeringen.nl/feeds/region/rotterdam-rijnmond/politie.rss").entries
 
     
     f = open("last.txt", "r")
     inlezen = f.read()
 
-    matches = ["ambu", "U bent", "proefalarm", "posten", "b1", "b2"]
 
-    for q in range(0, -1, -1):
 
-        time.sleep(3)
+        
 
-        if inlezen != feed[q].title and not any(word in feed[q].title for word in matches):
-            #print(feed[q])
-            my_datetime_str = feed[q].published
+    if inlezen != feed[0].title:
+        #print(feed[0])
+        my_datetime_str = feed[0].published
 
-            #print (my_datetime_str)
+        #print (my_datetime_str)
 
-            my_datetime_object = datetime.strptime(my_datetime_str, "%a, %d %b %Y %H:%M:%S %z")
+        my_datetime_object = datetime.strptime(my_datetime_str, "%a, %d %b %Y %H:%M:%S %z")
 
-            my_datetime_local = my_datetime_object.astimezone(pytz.timezone('Europe/Berlin')).strftime('%a, %d %b %Y %H:%M:%S %z')
+        my_datetime_local = my_datetime_object.astimezone(pytz.timezone('Europe/Berlin')).strftime('%a, %d %b %Y %H:%M:%S %z')
 
-            #print (my_datetime_local)
+        #print (my_datetime_local)
 
 
 
 
-            final_new = my_datetime_local [0:25] + '\n' + feed[q].title + '\n' + '#p2000 #rijnmond'
+        final_new = my_datetime_local [0:25] + '\n' + feed[0].title + '\n' + '#p2000 #rijnmond'
 
-            print(final_new)
+        print(final_new)
 
-            response = client.create_tweet(text=final_new)
+        response = client.create_tweet(text=final_new)
 
-            f = open('last.txt', 'w')
-            f.write(feed[q].title)
-            f.close()
+        f = open('last.txt', 'w')
+        f.write(feed[0].title)
+        f.close()
 
 parser()
