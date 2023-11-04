ea66e146e5c87376e6e33226562f3ffdf8458c4b
epronk88 <erik.pronk88@gmail.com>
2023-11-04 12:10:01 +0100

    0411_9

--- test.py
+++ test.py
@@ -23,22 +23,14 @@ def parser():
     feed = feedparser.parse("https://alarmeringen.nl/feeds/region/rotterdam-rijnmond/politie.rss").entries
 
 
-    for q in range(5, -1, -1):
-        print(feed[q].published + feed[q].title)
-
-
-
-
-
     
     f = open("last.txt", "r")
     inlezen = f.read()
 
-    matches = ["ambu", "U bent", "proefalarm", "posten", "b1", "b2"]
 
-    # for x in range(0, -1, -1):
 
-    if inlezen != feed[0].title: #and not any(word in feed[0].title for word in matches):
+
+    if inlezen != feed[0].title:
         #print(feed[x])
         my_datetime_str = feed[0].published
 
